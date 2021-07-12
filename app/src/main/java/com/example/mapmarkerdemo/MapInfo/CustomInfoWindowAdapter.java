195dfa3d3e2462e0272f637b9ed2827e9603b651
vkduy19 <vkduy19@apcs.vn>
2021-07-12 10:35:18 +0700

    InfoWindow + file structure

---
+++ app/src/main/java/com/example/mapmarkerdemo/MapInfo/CustomInfoWindowAdapter.java
@@ -0,0 +1,44 @@
+package com.example.mapmarkerdemo.MapInfo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mapmarkerdemo.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+    Context context;
+
+    public CustomInfoWindowAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        View contentView = LayoutInflater.from(context)
+                .inflate(R.layout.infowindow_content, null);
+
+        UniInfo uniInfo = (UniInfo)marker.getTag();
+
+        ImageView imageView = (ImageView)contentView.findViewById(R.id.logo);
+        imageView.setImageDrawable(context.getResources().getDrawable(uniInfo.getLogo()));
+
+        TextView titleView = (TextView)contentView.findViewById(R.id.title);
+        titleView.setText(uniInfo.getTitle());
+
+        TextView snippetView = (TextView)contentView.findViewById(R.id.snippet);
+        snippetView.setText(uniInfo.getInfo());
+
+        return contentView;
+    }
+}
